2e46ef77ca0812b20e9fddb45740ee6fefbaedfd
Jake Morrison <jake.morrison24@gmail.com>
2025-06-09 15:14:22 -0400

    Support geolocation and radius

---
+++ Geolocation.cs
@@ -0,0 +1,29 @@
+﻿using CrossPlatformLibrary.Geolocation;
+using Guards;
+using Microsoft.Practices.ServiceLocation;
+using System.Net;
+using Windows.Devices.Geolocation;
+
+namespace ADSBNotification
+{
+    public class Geolocation()
+    {
+        public static async Task<(double, double)> GetCurrentLocationLatLng()
+        {
+            var status = await Geolocator.RequestAccessAsync();
+            if (status == GeolocationAccessStatus.Allowed) {
+                var geolocator = new Geolocator();
+                geolocator.DesiredAccuracyInMeters = 50;
+                geolocator.AllowFallbackToConsentlessPositions();
+                var lkl = (await geolocator.GetGeopositionAsync()).Coordinate;
+                
+                if ((lkl.Longitude, lkl.Latitude) != (0, 0))
+                {
+                    return (lkl.Latitude, lkl.Longitude);
+                }
+            }
+
+            return (0, 0);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,48 +1,57 @@
 ﻿using ADSBNotification;
 using Microsoft.Toolkit.Uwp.Notifications;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
-using System.Threading.Tasks;
 
-Dictionary<string, DateTime> alreadyNotified = new Dictionary<string, DateTime>();
-HttpClient httpClient = new HttpClient();
-string host = Environment.GetEnvironmentVariable("ADSBNotifier_UltraFeederHost");
-string url = $"http://{host}/data/aircraft.json";
-string logoPath = Path.GetTempFileName();
-string logoUrl = $"http://{host}/images/tar1090-favicon.png";
+var alreadyNotified = new Dictionary<string, DateTime>();
+var httpClient = new HttpClient();
+var host = Environment.GetEnvironmentVariable("ADSBNotifier_UltraFeederHost");
+var stationLatLngEnv = Environment.GetEnvironmentVariable("ADSBNotifier_StationLatLng");
+var stationRadiusInKilometers = double.Parse(Environment.GetEnvironmentVariable("ADSBNotifier_RadiusInKilometers") ?? "5");
+var stationLatLng = stationLatLngEnv?.Split(',').Select(double.Parse).Chunk(2).Select(chunk => (chunk[0], chunk[1])).FirstOrDefault() ?? await Geolocation.GetCurrentLocationLatLng();
+var url = $"http://{host}/data/aircraft.json";
+var logoPath = Path.GetTempFileName();
+var logoUrl = $"http://{host}/images/tar1090-favicon.png";
+
 if (!string.IsNullOrEmpty(logoUrl))
 {
-    byte[] thumbnailBytes = await httpClient.GetByteArrayAsync(logoUrl);
+    var thumbnailBytes = await httpClient.GetByteArrayAsync(logoUrl);
     await File.WriteAllBytesAsync(logoPath, thumbnailBytes);
 }
+
 while (true)
 {
     try
     {
         Console.WriteLine("Checking for flights...");
-        HttpResponseMessage response = await httpClient.GetAsync(url);
+        var response = await httpClient.GetAsync(url);
         var aircraftResponse = await response.Content.ReadFromJsonAsync<AircraftResponse>(new JsonSerializerOptions()
         {
             PropertyNameCaseInsensitive = true
         });
 
-        Aircraft flightFlyingOver = aircraftResponse.Aircraft.FirstOrDefault(g => true);
+        var r_earth = 6378;
+        var flightFlyingOver = aircraftResponse.Aircraft.FirstOrDefault(g =>
+        {
+            var minLatitude = stationLatLng.Item1 + (-1 * stationRadiusInKilometers / r_earth) * (180 / Math.PI);
+            var maxLatitude = stationLatLng.Item1 + (stationRadiusInKilometers / r_earth) * (180 / Math.PI);
+            var minLongitude = stationLatLng.Item2 + (-1 * stationRadiusInKilometers / r_earth) * (180 / Math.PI) / Math.Cos(stationLatLng.Item1 * Math.PI / 180);
+            var maxLongitude = stationLatLng.Item2 + (stationRadiusInKilometers / r_earth) * (180 / Math.PI) / Math.Cos(stationLatLng.Item1 * Math.PI / 180);
+
+            return minLatitude <= g.Lat && g.Lat <= maxLatitude &&
+                   minLongitude <= g.Lon && g.Lon <= maxLongitude &&
+                   !alreadyNotified.ContainsKey(g.Flight ?? g.R);
+        });
         if (flightFlyingOver != null)
         {
-            string flightNo = flightFlyingOver.Flight ?? flightFlyingOver.R;
+            var flightNo = flightFlyingOver.Flight ?? flightFlyingOver.R;
             alreadyNotified.Add(flightNo, DateTime.Now);
-            string imageUrl = $"https://api.planespotters.net/pub/photos/hex/{flightFlyingOver.Hex}?reg={flightFlyingOver.R}&icaoType={flightFlyingOver.T}";
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(imageUrl);
-            PhotoResponse photoResponse = await HttpContentJsonExtensions.ReadFromJsonAsync<PhotoResponse>(httpResponseMessage.Content);
-            PhotoResponse responseImage = photoResponse;
-            string message = $"Flight {flightNo.Trim()} is now flying over";
+            var imageUrl = $"https://api.planespotters.net/pub/photos/hex/{flightFlyingOver.Hex}?reg={flightFlyingOver.R}&icaoType={flightFlyingOver.T}";
+            var httpResponseMessage = await httpClient.GetAsync(imageUrl);
+            var responseImage = await HttpContentJsonExtensions.ReadFromJsonAsync<PhotoResponse>(httpResponseMessage.Content);
+            var message = $"Flight {flightNo.Trim()} is now flying over";
             Console.WriteLine(message);
-            string imagePath = Path.GetTempFileName();
+            var imagePath = Path.GetTempFileName();
             if (responseImage?.photos != null && responseImage.photos.Count > 0)
             {
                 Photo photo = Enumerable.FirstOrDefault<Photo>(responseImage.photos);
@@ -51,25 +60,23 @@ while (true)
                     message = $"{message}{Environment.NewLine}{photo.photographer}";
                     if (!string.IsNullOrEmpty(photo.thumbnail?.src))
                     {
-                        byte[] thumbnailBytes = await httpClient.GetByteArrayAsync(photo.thumbnail_large.src);
+                        var thumbnailBytes = await httpClient.GetByteArrayAsync(photo.thumbnail_large.src);
                         await File.WriteAllBytesAsync(imagePath, thumbnailBytes);
                     }
                 }
             }
             message += $" {flightFlyingOver.Hex} at {flightFlyingOver.AltBaro} feet, {flightFlyingOver.Gs} knots, {flightFlyingOver.Track}° track.";
-            
+
             new ToastContentBuilder().AddText(message)
                 .SetToastScenario(ToastScenario.Default)
                 .AddHeroImage(new Uri(imagePath))
                 .SetProtocolActivation(new Uri($"http://{host}/?icao={flightFlyingOver.Hex}"))
                 .Show();
         }
-        foreach (KeyValuePair<string, DateTime> keyValuePair in alreadyNotified)
+        foreach (var val in alreadyNotified)
         {
-            KeyValuePair<string, DateTime> val = keyValuePair;
             if ((DateTime.Now - val.Value).TotalSeconds > 600.0)
                 alreadyNotified.Remove(val.Key);
-            val = new KeyValuePair<string, DateTime>();
         }
     }
     catch (Exception ex)
